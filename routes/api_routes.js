d845050ea7ae178f6538766f9b38001986f32497
DillEmma <emmabauta@gmail.com>
2021-01-26 15:46:04 -0500

    notes are added to side nav after submission

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -2,20 +2,34 @@ const path = require("path");
 const fs = require("fs");
 var notes = [];
 
-module.exports = function(app){
-    app.get("/api/notes", function(req, res){
-        fs.readFile(path.join(__dirname,"../db/db.json"), 'utf8', function (err, notes){
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        fs.readFile(path.join(__dirname, "../db/db.json"), 'utf8', function (err, data) {
             if (err) throw err;
-            console.log(notes);
-            return res.json(JSON.parse(notes));
-        })
-       
+            return res.json(JSON.parse(data));
+        });
     });
-    app.post("/api/notes", function(req, res){
-        fs.writeFile(path.join(__dirname, "../db/db.json"))
-        return notes;
+    app.post("/api/notes", function (req, res) {
+
+        fs.readFile(path.join(__dirname, "../db/db.json"), 'utf8', function (err, data) {
+            if (err) throw err;
+            var notes = JSON.parse(data)
+            console.log(notes);// update notes section
+            req.body.id = MATH.floor(Math.random()*1000)+1
+            notes.push(req.body);//add new note to the array
+
+
+            fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), function (err) {
+                if (err) throw err;
+                fs.readFile(path.join(__dirname, "../db/db.json"), 'utf8', function (err, data) {
+                    if (err) throw err;
+                    return res.json(JSON.parse(data));
+                });
+            });
+        });
     });
-    app.delete("/api/notes/:id", function(req, res){
+
+    app.delete("/api/notes/:id", function (req, res) {
         res.send();
     });
 
